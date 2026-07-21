f28d76297ad83f33efa9b42b84a11834133fcf95
TripAny Dev <dev@tripany.app>
2026-07-21 10:17:00 +0530

    fix: simplify vercel routes - API catch-all

--- api/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true, env: !!process.env.TURSO_DB_URL }));
-};
